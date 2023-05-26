a6aa188dcd1acb09e3efeefb8b5256a497867c97
SleepingAlbaricoque <tnqls0421@naver.com>
2023-05-26 09:28:11 +0900

    2023.05.28 html minor changes

---
+++ Kmarket1/member/js/gnb.js
@@ -0,0 +1,16 @@
+$(function(){
+    var gnb = $('#member_gnb > li > a');
+
+    gnb.click(function(e){
+        e.preventDefault();
+
+        var isOpen = $(this).next().is(':visible');
+        console.log(isOpen);
+
+        if(isOpen){
+            $(this).next().slideUp(200);
+        }else{
+            $(this).next().slideDown(200);
+        }
+    });
+});
